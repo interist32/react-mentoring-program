5d0aa56298a3b3971859b497009c6495002a803d
interist32-mi\interist32 <interist32@gmail.com>
2020-10-05 23:29:06 -0700

    Use formik for Add/Edit forms.

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -9,23 +9,35 @@ import { classNames } from '../../utils/utils';
 
 const Input = ({
     className,
+    name,
     value,
+    type = 'text',
     placeholder,
     onChange,
+    onBlur,
+    ...props
 }) => (
         <input {...classNames(
             'c-input',
             className,
         )}
-            type="text"
+            {...props}
+            name={name}
+            type={type}
             value={value}
             placeholder={placeholder}
-            onChange={(event) => onChange(event.target.value)} />
+            onChange={onChange}
+            onBlur={onBlur}
+        />
     );
 
 Input.propTypes = {
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    name: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+    type: PropTypes.oneOf(['date', 'text', 'number']),
 };
 
 export default Input;
--- src/containers/Home/AddMovieModal.jsx
+++ src/containers/Home/AddMovieModal.jsx
@@ -5,8 +5,7 @@ import Modal from '../../components/Modal/Modal';
 
 import './AddMovieModal.scss';
 
-import Input from '../../components/Input/Input';
-import Button from '../../components/Button/Button';
+import MovieForm from './MovieForm';
 import * as moviesActions from '../../store/actions/movies';
 import * as moviesSelectors from '../../store/selectors/movies';
 
@@ -15,81 +14,15 @@ const AddMovieModal = ({
     onCloseClick,
     dispatchAddMovie,
 }) => {
-    const [newMovieData, setNewMovieData] = useState({});
-
-    const handleInputOnChange = (type, value) => {
-        if (type === 'runtime') {
-            value = Number(value);
-        }
-        setNewMovieData({
-            ...newMovieData,
-            [type]: value,
-        });
-    };
-
-    const handleOnSaveClick = (event) => {
-        event.preventDefault();
-        dispatchAddMovie(newMovieData);
+    const handleSubmit = (movie) => {
+        dispatchAddMovie(movie);
         onCloseClick();
     };
 
-    const { title, overview, releaseDate, posterPath, genres, runtime } = newMovieData;
-
     return (
         <Modal onCloseClick={onCloseClick}>
-            <div className="home-add-movie">
-                <h1 className="tp-header home-add-movie__title">ADD MOVIE</h1>
-                <form className="home-add-movie-form">
-                    <label className="home-add-movie-form__label">
-                        Title
-                        <Input
-                            value={title}
-                            onChange={(value) => handleInputOnChange('title', value)}
-                        />
-                    </label>
-                    <label className="home-add-movie-form__label">
-                        Overview
-                        <Input
-                            value={overview}
-                            onChange={(value) => handleInputOnChange('overview', value)}
-                        />
-                    </label>
-                    <label className="home-add-movie-form__label">
-                        Release date
-                        <Input
-                            value={releaseDate}
-                            onChange={(value) => handleInputOnChange('releaseDate', value)}
-                        />
-                    </label>
-                    <label className="home-add-movie-form__label">
-                        Movie Image URL
-                        <Input
-                            value={posterPath}
-                            onChange={(value) => handleInputOnChange('posterPath', value)}
-                        />
-                    </label>
-                    <label className="home-add-movie-form__label">
-                        Genres
-                        <Input
-                            value={(genres || []).join(', ')}
-                            onChange={(value) => handleInputOnChange('genres', value.split(', '))}
-                        />
-                    </label>
-                    <label className="home-add-movie-form__label">
-                        Runtime
-                        <Input
-                            value={runtime}
-                            onChange={(value) => handleInputOnChange('runtime', value)}
-                        />
-                    </label>
-                    <div className="home-add-movie-form__actions">
-                        <Button type="reset" colorType="secondary">Reset</Button>
-                        <Button
-                            onClick={handleOnSaveClick}
-                            colorType="primary">Submit</Button>
-                    </div>
-                </form>
-            </div>
+            <h1 className="tp-header home-edit-movie__title">ADD MOVIE</h1>
+            <MovieForm onSubmit={handleSubmit} />
         </Modal>
     );
 }
