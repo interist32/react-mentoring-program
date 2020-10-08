1d9bad7c1134222c51d323b408017f3e95035203
interist32-mi\interist32 <interist32@gmail.com>
2020-10-07 22:57:08 -0700

    Fix Webpack to serve index.html for all routes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,16 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
+const path = require('path');
 
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    publicPath: '/',
+  },
   devServer: {
-    contentBase: './build',
-    historyApiFallback: {
-      index: 'index.html',
-    },
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
   }
 });
